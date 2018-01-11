5b44784a6b373725f62253fd98ba714af6da61c3
John Binning <john.binning17@gmail.com>
2018-01-10 17:46:50 -0700

    add bahn mi

--- src/MenuComponents/BahnMi.js
+++ src/MenuComponents/BahnMi.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 export const BahnMi = () => {
   return (
-    <div className='menu-cat'>Bahn Mi go here</div>
+    <section className='menu-cat' label='Banh Mi menu'>
+      <div className='menu-cat-header'>
+        <div id='menu-header-description'>
+          Tasty Baguette Sandwich topped with Cucumbers, Cilantro, Carrot & Radish Garnish & Jalapeño. Served with Vietnamese Chili-Lime Sauce.
+        </div>
+        <div id='description-addition'>(+$2.00 for Shrimp or Combination)</div>
+      </div>
+      <div className='menu-line'>
+        <div className='menu-item-name'>BÁNH MÍ – VIETNAMESE SANDWICH <div className='menu-item-description'>Choice Of Grilled Chicken, Pork, Beef, Or Tofu</div><div className='menu-modifier'>spicy</div></div>
+        <div className='price'>$5.95</div>
+      </div>
+    </section>
   )
 }
--- src/MenuComponents/Soup.js
+++ src/MenuComponents/Soup.js
@@ -4,49 +4,49 @@ export const Soup = () => {
   return (
     <div className='menu-cat'>
       <div className='menu-line'>
-        <div className='menu-item-description'></div>
+        <div className='menu-item-name'></div>
         <div className='price'>
           <span className='large'>LARGE</span>
           <span className='small'>SMALL</span>
         </div>
       </div>
       <div className='menu-line'>
-        <div className='menu-item-description'>HOT & SOUR SOUP<div className='menu-modifier'>Spicy</div></div>
+        <div className='menu-item-name'>HOT & SOUR SOUP<div className='menu-modifier'>Spicy</div></div>
         <div className='price'>
           <span className='small-price left-table'>$3.50</span>
           <span className='large-price right-table'>$5.95</span>
         </div>
       </div>
       <div className='menu-line'>
-        <div className='menu-item-description'>EGG DROP SOUP</div>
+        <div className='menu-item-name'>EGG DROP SOUP</div>
         <div className='price'>
           <span className='small-price left-table'>$3.50</span>
           <span className='large-price right-table'>$5.95</span>
         </div>
       </div>
       <div className='menu-line'>
-        <div className='menu-item-description'>WONTON SOUP</div>
+        <div className='menu-item-name'>WONTON SOUP</div>
         <div className='price'>
           <span className='small-price left-table'>$3.50</span>
           <span className='large-price right-table'>$6.25</span>
         </div>
       </div>
       <div className='menu-line'>
-        <div className='menu-item-description'>MISO SOUP</div>
+        <div className='menu-item-name'>MISO SOUP</div>
         <div className='price'>
           <span className='small-price left-table'>— </span>
           <span className='large-price right-table'>$6.50</span>
         </div>
       </div>
       <div className='menu-line'>
-        <div className='menu-item-description'>WONTON NOODLE SOUP</div>
+        <div className='menu-item-name'>WONTON NOODLE SOUP</div>
         <div className='price'>
           <span className='small-price left-table'>—</span>
           <span className='large-price right-table'>$9.95</span>
         </div>
       </div>
       <div className='menu-line'>
-        <div className='menu-item-description'>CHICKEN & SHRIMP RAMEN OR UDON</div>
+        <div className='menu-item-name'>CHICKEN & SHRIMP RAMEN OR UDON</div>
         <div className='price'>
           <span className='small-price left-table'>—</span>
           <span className='large-price right-table-2'>$12.95</span>

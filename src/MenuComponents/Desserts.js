6b8a9563a4ca95e53a4d3bd6eabcc8d58330de66
John Binning <john.binning17@gmail.com>
2018-01-10 17:53:46 -0700

    add desserts

--- src/MenuComponents/Desserts.js
+++ src/MenuComponents/Desserts.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 export const Desserts = () => {
   return (
-    <div className='menu-cat'>Desserts go here</div>
+    <div className='menu-cat'>
+      <div className='menu-line'>
+        <div className='menu-item-name'>MOCHI ICE CREAM</div>
+        <div className='price'>$5.25</div>
+      </div>
+      <div className='menu-line'>
+        <div className='menu-item-name'>MANGO STICKY RICE <div className='menu-item-description'>Drizzled With Coconut Cream</div></div>
+        <div className='price'>$6.50</div>
+      </div>
+    </div>
   )
 }

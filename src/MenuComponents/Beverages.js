9c8829e960ac880c185eaba3594778b8abe69f6c
John Binning <john.binning17@gmail.com>
2018-01-11 16:57:41 -0700

    add kids menu

--- src/MenuComponents/Beverages.js
+++ src/MenuComponents/Beverages.js
@@ -2,6 +2,31 @@ import React from 'react';
 
 export const Beverages = () => {
   return (
-    <div className='menu-cat'>Beverages go here</div>
+    <div className='menu-cat'>
+      <div className='menu-line'>
+        <div className='menu-item-name'>ICED OR HOT ESPRESSO COFFEE <div className='menu-item-description'>With Sweetened Condensed Milk</div></div>
+        <div className='price'>$3.95</div>
+      </div>
+      <div className='menu-line'>
+        <div className='menu-item-name'>FRESH-SQUEEZED LIMEADE</div>
+        <div className='price'>$3.95</div>
+      </div>
+      <div className='menu-line'>
+        <div className='menu-item-name'>COCONUT JUICE</div>
+        <div className='price'>$3.95</div>
+      </div>
+      <div className='menu-line'>
+        <div className='menu-item-name'>THAI ICED TEA</div>
+        <div className='price'>$3.50</div>
+      </div>
+      <div className='menu-line'>
+        <div className='menu-item-name'>HOT TEA</div>
+        <div className='price'>$2.95</div>
+      </div>
+      <div className='menu-line'>
+        <div className='menu-item-name'>ICED TEA</div>
+        <div className='price'>$2.95</div>
+      </div>
+    </div>
   )
 }

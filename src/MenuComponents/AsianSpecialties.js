70f77093e7505e754b17ce9f1353b5c2798a0c04
John Binning <john.binning17@gmail.com>
2018-01-16 22:55:55 -0700

    fix missing menu line div on Asian Specialties

--- src/MenuComponents/AsianSpecialties.js
+++ src/MenuComponents/AsianSpecialties.js
@@ -50,24 +50,30 @@ export const AsianSpecialties = () => {
           <div className='price'>$12.95</div>
         </div>
         <div>
-          <div className='menu-item-name'>THAI CURRY CHICKEN
-            <div className='menu-item-description'>(Stewed with vegetables in red-curry sauce. Topped with peanut garnish. (+$3 for Shrimp))</div>
-            <div className='menu-modifier'>Spicy</div>
-          <div className='price'>$12.95</div>
+          <div className='menu-line'>
+            <div className='menu-item-name'>THAI CURRY CHICKEN
+              <div className='menu-item-description'>(Stewed with vegetables in red-curry sauce. Topped with peanut garnish. (+$3 for Shrimp))</div>
+              <div className='menu-modifier'>Spicy</div>
+            <div className='price'>$12.95</div>
+          </div>
         </div>
         </div>
         <div>
-          <div className='menu-item-name'>PENANG CURRY CHICKEN
-            <div className='menu-item-description'>(Stewed with vegetables in Penang-curry sauce. Topped with peanut garnish. (+$3 for Shrimp))</div>
-            <div className='menu-modifier'>Spicy</div>
-          <div className='price'>$12.95</div>
+          <div className='menu-line'>
+            <div className='menu-item-name'>PENANG CURRY CHICKEN
+              <div className='menu-item-description'>(Stewed with vegetables in Penang-curry sauce. Topped with peanut garnish. (+$3 for Shrimp))</div>
+              <div className='menu-modifier'>Spicy</div>
+            <div className='price'>$12.95</div>
+          </div>
         </div>
         </div>
         <div>
-          <div className='menu-item-name'>STEAMED CHICKEN OR TOFU
-            <div className='menu-item-description'>(Freshly steamed with vegetables. (+$3 for Shrimp))</div>
-            <div className='menu-modifier'>Gluten Free</div>
-          <div className='price'>$12.95</div>
+          <div className='menu-line'>
+            <div className='menu-item-name'>STEAMED CHICKEN OR TOFU
+              <div className='menu-item-description'>(Freshly steamed with vegetables. (+$3 for Shrimp))</div>
+              <div className='menu-modifier'>Gluten Free</div>
+            <div className='price'>$12.95</div>
+          </div>
         </div>
         </div>
       </article>

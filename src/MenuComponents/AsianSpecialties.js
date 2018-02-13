27fbe4061ca67dbb176f55a7efc0f117d1265acc
John Binning <john.binning17@gmail.com>
2018-02-13 00:03:14 -0700

    update asian specialties description layout

--- src/MenuComponents/AsianSpecialties.js
+++ src/MenuComponents/AsianSpecialties.js
@@ -10,96 +10,96 @@ export const AsianSpecialties = () => {
       </div>
       <article id='menu-wrapper'>
         <div className='menu-line'>
-          <div className='menu-item-name'>SWEET & SOUR CHICKEN
+          <div className='as-item-name'>SWEET & SOUR CHICKEN
             <div className='menu-item-description'>(Lightly-battered chicken topped with sweet & sour glaze)</div>
             </div>
           <div className='price'>$12.95</div>
         </div>
         <div className='menu-line'>
-          <div className='menu-item-name'>TERIYAKI CHICKEN
+          <div className='as-item-name'>TERIYAKI CHICKEN
             <div className='menu-item-description'>(Grilled chicken breast tossed with ginger-teriyaki sauce)</div>
             </div>
           <div className='price'>$12.95</div>
         </div>
         <div className='menu-line'>
-          <div className='menu-item-name'>SESAME CHICKEN
+          <div className='as-item-name'>SESAME CHICKEN
             <div className='menu-item-description'>(Lightly-battered chicken tossed with our famous sesame-seed glaze)</div>
-            <div className='menu-modifier mm-spicy'>Spicy</div>
           </div>
+          <div className='menu-modifier mm-spicy'>Spicy</div>
           <div className='price'>$12.95</div>
         </div>
         <div className='menu-line'>
-          <div className='menu-item-name'>ORANGE CHICKEN
+          <div className='as-item-name'>ORANGE CHICKEN
             <div className='menu-item-description'>(Lightly-battered chicken tossed with spicy-tangy orange sauce)</div>
-            <div className='menu-modifier mm-spicy'>Spicy</div>
           </div>
+          <div className='menu-modifier mm-spicy'>Spicy</div>
           <div className='price'>$12.95</div>
         </div>
         <div className='menu-line'>
-          <div className='menu-item-name'>BASIL CHICKEN OR TOFU
+          <div className='as-item-name'>BASIL CHICKEN OR TOFU
             <div className='menu-item-description'>(Stir-fry with spicy Thai sauce & basil leaves. (+$3 for Shrimp))</div>
-            <div className='menu-modifier mm-spicy'>Spicy</div>
           </div>
+          <div className='menu-modifier mm-spicy'>Spicy</div>
           <div className='price'>$12.95</div>
         </div>
         <div className='menu-line'>
-          <div className='menu-item-name'>SRIRACHA CHICKEN OR TOFU
+          <div className='as-item-name'>SRIRACHA CHICKEN OR TOFU
             <div className='menu-item-description'>(Stir-fry tossed with vegetables & Sriracha sauce. (+$3 for Shrimp))</div>
-            <div className='menu-modifier mm-spicy'>Spicy</div>
           </div>
+          <div className='menu-modifier mm-spicy'>Spicy</div>
           <div className='price'>$12.95</div>
         </div>
         <div className='menu-line'>
-          <div className='menu-item-name'>THAI CURRY CHICKEN
+          <div className='as-item-name'>THAI CURRY CHICKEN
             <div className='menu-item-description'>(Stewed with vegetables in red-curry sauce. Topped with peanut garnish. (+$3 for Shrimp))</div>
-            <div className='menu-modifier mm-spicy'>Spicy</div>
-            <div className='price'>$12.95</div>
           </div>
+          <div className='menu-modifier mm-spicy'>Spicy</div>
+          <div className='price'>$12.95</div>
         </div>
         <div className='menu-line'>
-          <div className='menu-item-name'>PENANG CURRY CHICKEN
+          <div className='as-item-name'>PENANG CURRY CHICKEN
             <div className='menu-item-description'>(Stewed with vegetables in Penang-curry sauce. Topped with peanut garnish. (+$3 for Shrimp))</div>
-            <div className='menu-modifier mm-spicy'>Spicy</div>
-            <div className='price'>$12.95</div>
           </div>
+          <div className='menu-modifier mm-spicy'>Spicy</div>
+          <div className='price'>$12.95</div>
         </div>
         <div className='menu-line'>
-          <div className='menu-item-name'>STEAMED CHICKEN OR TOFU
+          <div className='as-item-name'>STEAMED CHICKEN OR TOFU
             <div className='menu-item-description'>(Freshly steamed with vegetables. (+$3 for Shrimp))</div>
-            <div className='menu-modifier mm-gf'>Gluten Free</div>
-            <div className='price'>$12.95</div>
           </div>
+          <div className='menu-modifier mm-gf'>Gluten Free</div>
+          <div className='price'>$12.95</div>
         </div>
         <div className='menu-line'>
-          <div className='menu-item-name'>FRIED RICE
+          <div className='as-item-name'>FRIED RICE
             <div className='menu-item-description'>Freshly-Cooked Rice Stir-Fried With Eggs, Onions, Peas & Carrots. Choice Of Chicken, Pork, Beef Or Vegetarian. Add $2.00 For Shrimp</div>
-            <div className='price'>$9.95</div>
           </div>
+          <div className='price'>$9.95</div>
         </div>
         <div className='menu-line'>
-          <div className='menu-item-name'>THAI FRIED RICE
+          <div className='as-item-name'>THAI FRIED RICE
             <div className='menu-item-description'>Freshly-Cooked Rice Stir-Fried With Peanuts, Eggs, Onions, Vegetables & Pineapple. Chicken & Shrimp</div>
-            <div className='menu-modifier mm-spicy'>Spicy</div>
-            <div className='price'>$12.95</div>
           </div>
+          <div className='menu-modifier mm-spicy'>Spicy</div>
+          <div className='price'>$12.95</div>
         </div>
         <div className='menu-line'>
-          <div className='menu-item-name'>LO-MEIN
+          <div className='as-item-name'>LO-MEIN
             <div className='menu-item-description'>Tasty Noodles Stir-Fried With Vegetables. Choice Of Chicken, Pork, Beef Or Vegetarian. Add $3.00 For Shrimp</div>
-            <div className='price'>$11.50</div>
           </div>
+          <div className='price'>$11.50</div>
         </div>
         <div className='menu-line'>
-          <div className='menu-item-name'>DOUBLE PAN-FRIED NOODLES
+          <div className='as-item-name'>DOUBLE PAN-FRIED NOODLES
             <div className='menu-item-description'>Crispy Egg Noodles Topped With Stir-Fried Vegetables. Chicken & Shrimp</div>
-            <div className='price'>$13.50</div>
           </div>
+          <div className='price'>$13.50</div>
         </div>
         <div className='menu-line'>
-          <div className='menu-item-name'>PAD THAI
+          <div className='as-item-name'>PAD THAI
             <div className='menu-item-description'>Rice Noodles Stir-Fried With Eggs, Onions, Vegetables & Topped With Bean Sprouts & Peanut Garnish. Chicken & Shrimp.</div>
-            <div className='price'>$13.50</div>
           </div>
+          <div className='price'>$13.50</div>
         </div>
       </article>
     </section>
